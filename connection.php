f8ea08f5549468cdcbe39084030e5c4cdc7e8095
yasser12333122 <ayoubyaser89@gmail.com>
2025-05-27 22:08:03 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $server = "dbb.mysql.database.azure.com";
 $username = "admin2";
 $password = "admin123##";
-$db = "blog";
+$db = "dbb";
 try {
     $pdo = new PDO("mysql:host=$server;dbname=$db", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
